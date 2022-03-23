b36c7af9ee96e88f8db59e5da106782a172b5596
Jano Jamin <jano.jamin@student.ap.be>
2022-03-23 16:27:36 +0100

    Summary pagina + css

--- index.ts
+++ index.ts
@@ -1,20 +1,30 @@
 const express = require('express');
-const ejs= require('ejs');
+const ejs = require('ejs');
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('port', 3000);
 
-app.get('/',(req:any, res:any)=>{
+app.use('/public', express.static('public'));
+
+app.get('/', (req: any, res: any) => {
     res.render('main.ejs')
 })
 
-app.get('/reference', (req:any, res:any)=>{
+app.get('/reference', (req: any, res: any) => {
     res.render('reference.ejs')
 })
 
-app.use(function(req:any,res:any){
+app.get('/summary', (req: any, res: any) => {
+    res.render('summary.ejs')
+})
+
+app.get('/header', (req: any, res: any) => {
+    res.render('header.ejs')
+})
+
+app.use(function (req: any, res: any) {
     res.status(404).render('404.ejs');
 });
 
-app.listen(app.get('port'), ()=>console.log( '[server] http://localhost:' + app.get('port')));
+app.listen(app.get('port'), () => console.log('[server] http://localhost:' + app.get('port')));
---
+++ public/assets/style.cs
@@ -0,0 +1,30 @@
+.menu {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: yellow;
+}
+
+
+.menu li {
+  float: left;
+}
+
+.menu li a {
+  display: block;
+  color: red;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.summary, .summary th, .summary td{
+  border: 1px solid
+}
+
+.summary img{
+  max-widht: 200px;
+  height: 200px;
+  padding: 5%;
+}
