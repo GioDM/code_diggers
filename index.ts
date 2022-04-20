0b2d6d6311dca0136dd4a335a6996fd7cbc3a20b
SOJKE CALUWE <s096538@ap.be>
2022-04-20 17:58:01 +0200

    api variabel maken + post function toegevoegd

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const axios = require('axios');
+
+let apiData:any;
+
+const getApi = async (api : string):Promise<void> => {
+    let result = await axios.get(`https://rebrickable.com/api/v3/lego/${api}/?key=6cd12548f2028a329b97cc9f1aa3899f`);
+    apiData = result.data;
+};
 
 app.set('view engine', 'ejs');
 app.set('port', 3000);
 
-
 app.use('/public', express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended:true}))
 
 app.get('/',(req:any, res:any)=>{
     res.render('projects.ejs', { title: 'IT Project | Projecten' })
@@ -23,8 +32,16 @@ app.get('/legomasters/minifig', (req:any, res:any)=>{
 app.get('/legomasters/blacklist', (req:any, res:any)=>{
     res.render('legomasters/overzichtBlacklist.ejs', { title: 'LegoMasters | Blacklist' })
 })
-app.get('/legomasters/sort/sort', (req:any, res:any)=>{
-    res.render('legomasters/sort/ordenenMain.ejs', { title: 'LegoMasters | Sorting' })
+app.post('/legomasters/sort/sort', (req:any, res:any)=>{
+    let aantalMinifig = req.body.minifig;
+    getApi("minifigs");
+    console.log(apiData);
+    res.render('legomasters/sort/ordenenMain.ejs', { 
+        title: 'LegoMasters | Sorting',
+        //aantal : aantalMinifig,
+        //pic: minifigCode
+        //number : minifigCode
+    })
 })
 app.get('/legomasters/sort/result', (req:any, res:any)=>{
     res.render('legomasters/sort/resultaat.ejs', { title: 'LegoMasters | Sorting Result' })
